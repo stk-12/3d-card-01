ae843d5335a69b3131fd7b2d570893b22f04ee61
keisuke sato <corydoras121@gmail.com>
2025-02-24 01:09:40 +0900

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ class Card3D {
   constructor(element) {
     this.card = element;
     this.bounds = this.card.getBoundingClientRect();
-    this.glow = this.card.querySelector('.glow');
+    this.glow = this.card.querySelector('.js-card-glow');
+    this.effect = this.card.querySelector('.js-card-effect');
 
     this.card.addEventListener('mouseenter', this.onMouseEnter.bind(this));
     this.card.addEventListener('mouseleave', this.onMouseLeave.bind(this));
@@ -10,6 +11,13 @@ class Card3D {
 
   onMouseEnter() {
     this.bounds = this.card.getBoundingClientRect();
+
+    gsap.to(this.effect, {
+      '--opacity': 1,
+      duration: 0.4,
+      ease: 'power2.out'
+    });
+
     document.addEventListener('mousemove', this.onMouseMove);
   }
 
@@ -23,6 +31,9 @@ class Card3D {
     };
     const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
 
+    this.card.style.setProperty('--mouse-x', `${leftX}px`);
+    this.card.style.setProperty('--mouse-y', `${topY}px`);
+
     this.card.style.transform = `
       
       rotate3d(
@@ -33,23 +44,29 @@ class Card3D {
       )
     `;
 
-    this.glow.style.backgroundImage = `
-      radial-gradient(
-        circle at
-        ${center.x * 2 + this.bounds.width / 2}px
-        ${center.y * 2 + this.bounds.height / 2}px,
-        #ffffff55,
-        #0000000f
-      )
-    `;
+    // this.glow.style.backgroundImage = `
+    //   radial-gradient(
+    //     circle at
+    //     ${center.x * 2 + this.bounds.width / 2}px
+    //     ${center.y * 2 + this.bounds.height / 2}px,
+    //     #ffffff55,
+    //     #0000000f
+    //   )
+    // `;
   };
 
   onMouseLeave = () => {
     document.removeEventListener('mousemove', this.onMouseMove);
     this.card.style.transform = '';
     this.glow.style.backgroundImage = '';
+
+    gsap.to(this.effect, {
+      '--opacity': 0,
+      duration: 0.4,
+      ease: 'power2.out'
+    });
   };
 }
 
 // すべての .card 要素に 3D エフェクトを適用
-document.querySelectorAll('.card').forEach(card => new Card3D(card));
+document.querySelectorAll('.js-card').forEach(card => new Card3D(card));
